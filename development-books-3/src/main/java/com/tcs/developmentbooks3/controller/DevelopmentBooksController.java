ea339c82eeb03e7ae3e6587331669fdd6dc0214c
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-17 19:24:02 +0530

    feat: get all books should return all book with details.

--- development-books-3/src/main/java/com/tcs/developmentbooks3/controller/DevelopmentBooksController.java
+++ development-books-3/src/main/java/com/tcs/developmentbooks3/controller/DevelopmentBooksController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tcs.developmentbooks3.model.Books;
 import com.tcs.developmentbooks3.service.BookService;
 
 @RestController
@@ -13,9 +14,9 @@ public class DevelopmentBooksController {
 
 	@Autowired
 	BookService service;
-
-	@GetMapping("/getAllBooks")
-	public List<String> getAllBooks() {
+	
+    @GetMapping("/getAllBooks")
+    public List<Books> getAllBooks() {
 		return service.getAllBooks();
-	}
+    }
 }
--- development-books-3/src/test/java/com/tcs/developmentbooks3/controller/DevelopmentBooksControllerTest.java
+++ development-books-3/src/test/java/com/tcs/developmentbooks3/controller/DevelopmentBooksControllerTest.java
@@ -1,13 +1,13 @@
 package com.tcs.developmentbooks3.controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.tcs.developmentbooks3.model.Books;
 
 class DevelopmentBooksControllerTest {
 
@@ -20,19 +20,14 @@ class DevelopmentBooksControllerTest {
 
 	@Test
 	void getBookNameShouldReturnNameOfTheBook() {
-		List<String> result = controller.getAllBooks();
+		List<Books> result = controller.getAllBooks();
 		assertEquals("Clean Code", result.get(0));
 	}
 
 	@Test
 	public void getAllBooksShouldReturnFiveBookNames() {
-		List<String> books = controller.getAllBooks();
+		List<Books> books = controller.getAllBooks();
 		assertEquals(5, books.size());
-		assertTrue(books.containsAll(getAllBooks()));
 	}
 
-	private List<String> getAllBooks() {
-		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
-				"Test Driven Development by Example", "Working Effectively With Legacy Code");
-	}
 }
