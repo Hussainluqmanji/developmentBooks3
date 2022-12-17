acb3df12c896a88cf9f98fdbc9f847d1f6593492
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-17 16:13:50 +0530

    chore: create springboot project for development books.

---
+++ development-books-3/src/main/java/com/tcs/developmentbooks3/DevelopmentBooks3Application.java
@@ -0,0 +1,13 @@
+package com.tcs.developmentbooks3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooks3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooks3Application.class, args);
+	}
+
+}
