745b12162e6b0cec21930e6fbf21fff5df751ce6
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-17 17:57:11 +0530

    refactor: added enum for book names.

--- development-books-3/src/test/java/com/tcs/developmentbooks3/service/BookServiceTest.java
+++ development-books-3/src/test/java/com/tcs/developmentbooks3/service/BookServiceTest.java
@@ -1,7 +1,9 @@
 package com.tcs.developmentbooks3.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,12 @@ class BookServiceTest {
 		BookService service = new BookService();
 		List<String> books = service.getAllBooks();
 		assertEquals(5, books.size());
+		assertTrue(books.containsAll(getAllBooks()));
+	}
+
+	private List<String> getAllBooks() {
+		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
+				"Test Driven Development by Example", "Working Effectively With Legacy Code");
 	}
 
 }
